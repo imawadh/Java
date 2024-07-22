eeeaddc4ad55c3da6bcef0229cbcd59600800573
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2024-07-22 19:22:33 +0530

    Control Construct Fizz Buzz Game

---
+++ Programs/FizzBuzzGame.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class FizzBuzzGame{
+    public static void main(String [] args)
+    {
+        System.out.print("Enter a nuber :: ");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if(num%3==0 && num%5==0)
+        {
+            System.out.println("FizzBuzz");
+        }
+        else if(num%3==0){
+            System.out.println("Fizz");
+        }
+        else if(num%5==0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(num);
+        }
+    }
+}
