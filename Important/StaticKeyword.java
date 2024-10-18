a1d23175ce72178da3ad63fb9d9b0c6aef29b8e9
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2024-10-18 09:04:13 +0530

    Random

---
+++ Important/StaticKeyword.java
@@ -0,0 +1,13 @@
+// package Important;
+
+class Dog {    
+    static void bark() {        
+        System.out.println("Bark !");    
+    }
+}
+
+public class StaticKeyword {    
+    public static void main(String[] args) {        
+        Dog.bark();    
+    }
+}
